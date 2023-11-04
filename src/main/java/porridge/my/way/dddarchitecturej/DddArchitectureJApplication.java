23ddde354c597c8971a8c3e6998ba04d69a6267c
PorriDG3 <gn02053231@gmail.com>
2023-11-04 23:20:17 +0800

    feat: Init

---
+++ src/main/java/porridge/my/way/dddarchitecturej/DddArchitectureJApplication.java
@@ -0,0 +1,13 @@
+package porridge.my.way.dddarchitecturej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddArchitectureJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddArchitectureJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/porridge/my/way/dddarchitecturej/DddArchitectureJApplicationTests.java
@@ -0,0 +1,13 @@
+package porridge.my.way.dddarchitecturej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddArchitectureJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
