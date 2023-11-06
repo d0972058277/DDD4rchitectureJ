f2e301d482d6433faafcd0ae6b36a482028ee443
PorriDG3 <gn02053231@gmail.com>
2023-11-06 22:41:45 +0800

    style: change TNotification to N

--- src/main/java/porridge/my/way/dddarchitecturej/architecture/core/IDomainEventHandler.java
+++ src/main/java/porridge/my/way/dddarchitecturej/architecture/core/IDomainEventHandler.java
@@ -2,5 +2,5 @@ package porridge.my.way.dddarchitecturej.architecture.core;
 
 import an.awesome.pipelinr.Notification;
 
-public interface IDomainEventHandler<TNotification extends IDomainEvent> extends Notification.Handler<TNotification> {
+public interface IDomainEventHandler<N extends IDomainEvent> extends Notification.Handler<N> {
 }
