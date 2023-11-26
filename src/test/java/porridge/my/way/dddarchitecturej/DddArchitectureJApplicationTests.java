6e287cefd1ae9452b2113031ee42e9a8abb9a63f
PorriDG3 <gn02053231@gmail.com>
2023-11-26 20:35:42 +0800

    refactor: remove unused imports in test file

--- src/test/java/porridge/my/way/dddarchitecturej/DddArchitectureJApplicationTests.java
+++ src/test/java/porridge/my/way/dddarchitecturej/DddArchitectureJApplicationTests.java
@@ -1,18 +1,7 @@
 package porridge.my.way.dddarchitecturej;
 
-import java.math.BigDecimal;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import porridge.my.way.dddarchitecturej.order.domain.models.CustomerInfo;
-import porridge.my.way.dddarchitecturej.order.domain.models.Order;
-import porridge.my.way.dddarchitecturej.order.domain.models.OrderItem;
-import porridge.my.way.dddarchitecturej.order.infrastructure.models.OrderDto;
 
 @SpringBootTest
 class DddArchitectureJApplicationTests {
