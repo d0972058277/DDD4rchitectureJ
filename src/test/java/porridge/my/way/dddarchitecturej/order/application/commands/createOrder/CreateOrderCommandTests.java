d66389a448cf5f90d981044169f6a5775080f8ed
PorriDG3 <gn02053231@gmail.com>
2024-01-20 16:53:42 +0800

    refactor: Adjust the name of execute/fetch in mediator to send.

--- src/test/java/porridge/my/way/dddarchitecturej/order/application/commands/createOrder/CreateOrderCommandTests.java
+++ src/test/java/porridge/my/way/dddarchitecturej/order/application/commands/createOrder/CreateOrderCommandTests.java
@@ -1,18 +1,17 @@
 package porridge.my.way.dddarchitecturej.order.application.commands.createOrder;
 
-import static org.mockito.Mockito.times;
-
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-import jakarta.persistence.EntityManager;
 import porridge.my.way.dddarchitecturej.architecture.shell.cqrs.IMediator;
 import porridge.my.way.dddarchitecturej.order.application.repositories.IOrderRepository;
 import porridge.my.way.dddarchitecturej.order.domain.models.CustomerInfo;
 import porridge.my.way.dddarchitecturej.order.domain.models.Order;
+
+import static org.mockito.Mockito.times;
 
 @SpringBootTest
 public class CreateOrderCommandTests {
@@ -29,7 +28,7 @@ public class CreateOrderCommandTests {
         CreateOrderCommand createOrderCommand = new CreateOrderCommand(CustomerInfo.create("name", "address"));
 
         // When
-        mediator.execute(createOrderCommand);
+        mediator.send(createOrderCommand);
 
         // Then
         Mockito.verify(entityManager, times(1)).persist(Mockito.any(Order.class));
