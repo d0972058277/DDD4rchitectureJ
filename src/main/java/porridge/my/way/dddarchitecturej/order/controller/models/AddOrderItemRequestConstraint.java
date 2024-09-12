e11d845e5e23150225b08ef0600ca93f81ce959b
PorriDG3 <gn02053231@gmail.com>
2024-09-12 12:03:43 +0800

    ♻️ refactor: removed unused imports and default values in constraints

--- src/main/java/porridge/my/way/dddarchitecturej/order/controller/models/AddOrderItemRequestConstraint.java
+++ src/main/java/porridge/my/way/dddarchitecturej/order/controller/models/AddOrderItemRequestConstraint.java
@@ -3,7 +3,6 @@ package porridge.my.way.dddarchitecturej.order.controller.models;
 import jakarta.validation.Constraint;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import jakarta.validation.Payload;
 import porridge.my.way.dddarchitecturej.architecture.exceptions.IllegalArgumentDomainException;
 import porridge.my.way.dddarchitecturej.order.domain.models.OrderItem;
 
@@ -14,12 +13,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = AddOrderItemRequestConstraint.Validator.class)
 public @interface AddOrderItemRequestConstraint {
-    String message() default "Request constraint violated";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
-
     class Validator implements ConstraintValidator<AddOrderItemRequestConstraint, AddOrderItemRequest> {
         @Override
         public boolean isValid(AddOrderItemRequest value, ConstraintValidatorContext context) {
--- src/main/java/porridge/my/way/dddarchitecturej/order/controller/models/CreateOrderRequestConstraint.java
+++ src/main/java/porridge/my/way/dddarchitecturej/order/controller/models/CreateOrderRequestConstraint.java
@@ -3,7 +3,6 @@ package porridge.my.way.dddarchitecturej.order.controller.models;
 import jakarta.validation.Constraint;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import jakarta.validation.Payload;
 import org.springframework.stereotype.Component;
 import porridge.my.way.dddarchitecturej.architecture.exceptions.IllegalArgumentDomainException;
 import porridge.my.way.dddarchitecturej.order.domain.models.CustomerInfo;
@@ -15,12 +14,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = CreateOrderRequestConstraint.Validator.class)
 public @interface CreateOrderRequestConstraint {
-    String message() default "Request constraint violated";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
-
     @Component
     class Validator implements ConstraintValidator<CreateOrderRequestConstraint, CreateOrderRequest> {
         @Override
